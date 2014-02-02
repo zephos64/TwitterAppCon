9ab00097ba7cd5807397c1e251d11b544804185e
ryan <ryanwhitworth@sbcglobal.net>
2014-02-01 20:38:01 -0800

    tweets won't send if over limit (no functionality still for sending)

--- TwitterApp/src/com/codepath/apps/twitterapp/ComposeTweet.java
+++ TwitterApp/src/com/codepath/apps/twitterapp/ComposeTweet.java
@@ -1,15 +1,33 @@
 package com.codepath.apps.twitterapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ComposeTweet extends Activity {
+	
+	private EditText etComposeTweet;
+	private TextView tvCharLeftCounter;
+	private Button button1;
+	
+	private int defaultCharLimit = 140;
+	private boolean disableComp = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_compose_tweet);
+		
+		etComposeTweet = (EditText) findViewById(R.id.etComposeTweet);
+		tvCharLeftCounter = (TextView) findViewById(R.id.tvCharLeftCounter);
+		button1 = (Button) findViewById(R.id.button1);
+		
+		setupViewListener();
 	}
 
 	@Override
@@ -19,4 +37,37 @@ public class ComposeTweet extends Activity {
 		return true;
 	}
 
+	private void setupViewListener() {
+		etComposeTweet.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				tvCharLeftCounter.setText(String.valueOf(defaultCharLimit - etComposeTweet.length()));
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				if(disableComp == false &&
+						(etComposeTweet.length() > defaultCharLimit)) {
+					//Disable functionality, tweet too long
+					button1.setEnabled(false);
+					tvCharLeftCounter.setTextColor(
+							android.graphics.Color.RED);
+					disableComp = true;
+				} else if (disableComp == true &&
+						(etComposeTweet.length() <= defaultCharLimit)) {
+					tvCharLeftCounter.setTextColor(
+							android.graphics.Color.DKGRAY);
+					
+					button1.setEnabled(true);
+				}
+			}
+		});
+	}
 }
